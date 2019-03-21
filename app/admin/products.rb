7dd6060285c15eaac0fe1a5dd43af7becd2d40ce
Chris Ragasa <tofuragasa@gmail.com>
2019-03-20 22:42:05 -0500

    Added image column to Products table

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Product do
-  permit_params :name, :price, :type, :description, :category_id
+  permit_params :name, :price, :type, :description, :category_id, :image
 end
