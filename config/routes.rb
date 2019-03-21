960e810cd379e18c46975ef1f6cd5a05719dd97a
Chris Ragasa <tofuragasa@gmail.com>
2019-03-21 10:11:24 -0500

    Created header and nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :products, only: [:index]
-  resources :categories, only: [:index]
+  root to: 'products#index'
+  resources :products, only: %i[index show]
+  resources :categories, only: %i[index show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
