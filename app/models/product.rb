dab723d8f650d9e229426fc1863dbc68a1112280
Chris Ragasa <tofuragasa@gmail.com>
2019-03-20 23:14:57 -0500

    Added mini magick gem to create thumnail images in carrier wave

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ApplicationRecord
   belongs_to :category
   validates :name, :price, :description, presence: true
   validates :price, numericality: true
+
+  mount_uploader :image, ImageUploader
 end
