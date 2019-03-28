6582520114bb2551cca15e77d8ac8a861e5b88e7
Chris Ragasa <tofuragasa@gmail.com>
2019-03-27 22:58:10 -0500

    Added associations between Customers and Provinces

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
-  # belongs_to :province
-  validates :fullname, :phone, :address, :city, :postalcode, presence: true
+  belongs_to :province
+  validates :fullname, :phone, :address, :city, :postalcode, :province_id presence: true
   canadian_postal_code = /\A[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1}[ -]?\d{1}[A-Z]{1}\d{1}\z/
   validates :postalcode, format: { with: canadian_postal_code }
 end
