5d5150a5cf3c57bf1cf56e16ae9e7d50a0414774
Chris Ragasa <tofuragasa@gmail.com>
2019-03-27 23:10:49 -0500

    Performed initial configuration for devise

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@
 
 class Customer < ApplicationRecord
   belongs_to :province
-  validates :fullname, :phone, :address, :city, :postalcode, :province_id presence: true
+  validates :fullname, :phone, :address, :city, :postalcode, :province_id, presence: true
   canadian_postal_code = /\A[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1}[ -]?\d{1}[A-Z]{1}\d{1}\z/
   validates :postalcode, format: { with: canadian_postal_code }
 end
