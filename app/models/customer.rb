06796744a720237fc35a5de9492de1c92c887304
Chris Ragasa <tofuragasa@gmail.com>
2019-03-27 22:41:55 -0500

    Added customer to active admin and added validation.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Customer < ApplicationRecord
+  # belongs_to :province
+  validates :fullname, :phone, :address, :city, :postalcode, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_033647) do
+ActiveRecord::Schema.define(version: 2019_03_28_033611) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 2019_03_21_033647) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "fullname"
+    t.string "phone"
+    t.string "address"
+    t.string "city"
+    t.string "postalcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
