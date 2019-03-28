7d4d9f9447eaee9d35a5897176153fd81926e092
Chris Ragasa <tofuragasa@gmail.com>
2019-03-28 10:37:41 -0500

    Modifed customers edit page with proper customer info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:fullname, :phone, :address, :province_id, :city, :postalcode, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fullname, :phone, :address, :province_id, :city, :postalcode, :email, :password) }
+  end
 end
