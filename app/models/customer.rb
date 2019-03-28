48f8c81d91245d93f8805f388540a8d86615e627
Chris Ragasa <tofuragasa@gmail.com>
2019-03-27 22:49:07 -0500

    Added additional validation to Customers postalcode

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,6 @@
 class Customer < ApplicationRecord
   # belongs_to :province
   validates :fullname, :phone, :address, :city, :postalcode, presence: true
+  canadian_postal_code = /\A[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1}[ -]?\d{1}[A-Z]{1}\d{1}\z/
+  validates :postalcode, format: { with: canadian_postal_code }
 end
