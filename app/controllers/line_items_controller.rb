654782643dc6aed42f952bc0a700caacaffe7791
Chris Ragasa <tofuragasa@gmail.com>
2019-04-06 16:15:56 -0500

    Added a quantity column to lineitems

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,38 @@
 # frozen_string_literal: true
 
 class LineItemsController < ApplicationController
-  
+  include CurrentCart
+  before_action :set_line_item, only: %i[show edit update destroy]
+  before_action :set_cart, only: [:create]
+
+  def index
+    @line_item = LineItem.all
+  end
+
+  def show; end
+
+  def new
+    @line_item = LineItem.new
+  end
+
+  def edit; end
+
+  def create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(line_item_params)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to @line_item.cart, notice: 'Item added to cart.' }
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { render :new }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update; end
+
+  def destroy; end
 end
