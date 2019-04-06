972c0013246e46bc3be75e54b57082370ed930ef
Chris Ragasa <tofuragasa@gmail.com>
2019-04-06 18:35:04 -0500

    Added cart link on nav when there are items in the cart.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def cart_count_over_one
+    "<span> #{@cart.total_qty}</span>".html_safe if @cart.line_items.count > 0
+  end
+
+  def cart_has_items
+    @cart.line_items.count > 0
+  end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -5,6 +5,10 @@ class LineItem < ApplicationRecord
   belongs_to :cart
 
   def total_price
-    product.price.to_i * quantity.to_i
+    product.price * quantity
+  end
+
+  def total_qty
+    quantity.to_i
   end
 end
